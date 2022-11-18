e2ce4ecaf2a5a8b6141ddcf47cfcf695f38cc948
Prorok <frolov_vovan@mail.ru>
2022-11-18 16:40:26 +0300

    deleted unused imports

--- src/main/java/com/kata/trade_accounting/exception/AlreadyInUseException.java
+++
@@ -1,7 +0,0 @@
-package com.kata.trade_accounting.exception;
-
-public class AlreadyInUseException extends RuntimeException{
-    public AlreadyInUseException(String message) {
-        super(message);
-    }
-}
