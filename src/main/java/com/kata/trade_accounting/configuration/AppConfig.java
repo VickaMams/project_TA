145ba5e4878c344df03d0a4de0dbfb49a668988b
NAilerStrikes228 <47347992+NAilerStrikes228@users.noreply.github.com>
2022-11-23 14:52:05 +0300

    edit model Worker, add swagger in controller

--- src/main/java/com/kata/trade_accounting/configuration/AppConfig.java
+++ src/main/java/com/kata/trade_accounting/configuration/AppConfig.java
@@ -1,14 +1,7 @@
 package com.kata.trade_accounting.configuration;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableSwagger2
 public class AppConfig {
