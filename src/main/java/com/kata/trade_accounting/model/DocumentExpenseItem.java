f700cdae3d3688916724f5c842883d510e9afd3e
Robert <56319990+Acc3l3rat0r@users.noreply.github.com>
2022-11-26 14:34:43 +0300

    document added

---
+++ src/main/java/com/kata/trade_accounting/model/DocumentExpenseItem.java
@@ -0,0 +1,11 @@
+package com.kata.trade_accounting.model;
+
+public enum DocumentExpenseItem {
+    REFUND,
+    PURCHASE_OF_GOODS,
+    TAXES_AND_FEES,
+    MOVEMENT,
+    RENT,
+    SALARY,
+    MARKETING_AND_ADVERTISING
+}
---
+++ src/main/java/com/kata/trade_accounting/model/DocumentStatus.java
@@ -0,0 +1,11 @@
+package com.kata.trade_accounting.model;
+
+public enum DocumentStatus {
+    NEW,
+    CONFIRMED,
+    ASSEMBLED,
+    SHIPPED,
+    DELIVERED,
+    REFUND,
+    CANCELED
+}
