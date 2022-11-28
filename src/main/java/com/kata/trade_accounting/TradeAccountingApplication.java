3c051b8f6040ecb834bd0f35c2e01e2dc44cb2b4
panch <panchenkod2002@mail.ry>
2022-11-28 17:47:37 +0300

    task changed

--- src/main/java/com/kata/trade_accounting/TradeAccountingApplication.java
+++ src/main/java/com/kata/trade_accounting/TradeAccountingApplication.java
@@ -7,13 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TradeAccountingApplication {
 
-
-
     public static void main(String[] args) {
         SpringApplication.run(TradeAccountingApplication.class, args);
-
     }
-
-
 
 }
