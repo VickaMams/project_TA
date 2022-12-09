5d6369dde07e0930ad5453eb03672d43baed293c
Victoria2412 <nado122012@gmail.com>
2022-12-09 15:19:25 +0300

    Product_packing_03

--- src/main/java/com/kata/trade_accounting/mapper/ProductPackingMapper.java
+++ src/main/java/com/kata/trade_accounting/mapper/ProductPackingMapper.java
@@ -2,12 +2,12 @@ package com.kata.trade_accounting.mapper;
 
 import com.kata.trade_accounting.dto.ProductPackagingDto;
 import com.kata.trade_accounting.model.ProductPackaging;
-import jdk.incubator.vector.VectorOperators;
 import org.mapstruct.Mapper;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Mapper(componentModel = "spring")
 @Component
-public interface ProductPackingMapper extends VectorOperators.Conversion<ProductPackagingDto, ProductPackaging> {
+public interface ProductPackingMapper extends Converter<ProductPackagingDto, ProductPackaging> {
      ProductPackaging toProductPackaging(ProductPackagingDto productPackagingDto);
 }
